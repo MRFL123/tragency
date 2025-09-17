34aa43e991e4bdf7e9bc254cf3a713a086998d10
Ahmed Mostafa <ahmedmostafa20002@yahoo.com>
2025-09-17 14:14:00 +0300

    fix

--- framework/builder/acf-blocks/video-section.php
+++ framework/builder/acf-blocks/video-section.php
@@ -1,3 +1,76 @@
 <?php
+add_action( 'acf/include_fields', function() {
+	if ( ! function_exists( 'acf_add_local_field_group' ) ) {
+		return;
+	}
+
+	acf_add_local_field_group( array(
+	'key' => 'group_68b5e8f62fe84',
+	'title' => 'video section',
+	'fields' => array(
+		array(
+			'key' => 'field_68b5e8f6abd9d',
+			'label' => 'Heading',
+			'name' => 'heading',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'allow_in_bindings' => 0,
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+		),
+		array(
+			'key' => 'field_68b5e907abd9e',
+			'label' => 'Video',
+			'name' => 'video',
+			'aria-label' => '',
+			'type' => 'file',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'return_format' => 'array',
+			'library' => 'all',
+			'min_size' => '',
+			'max_size' => '',
+			'mime_types' => '',
+			'allow_in_bindings' => 1,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'block',
+				'operator' => '==',
+				'value' => 'acf/video-section',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+	'show_in_rest' => 0,
+) );
+} );
+
 
 ?>
