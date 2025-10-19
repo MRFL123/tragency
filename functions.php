d46dadd6c33cbf41b2ae2c900be110aeeaf2db82
Abanoub-N <abanoubnabil799@gmail.com>
2025-10-19 14:19:38 +0300

    Fix pagination

--- functions.php
+++ functions.php
@@ -64,22 +64,24 @@ collect(['setup', 'filters'])
 
 
 function fix_product_category_pagination() {
+    add_rewrite_tag('%product-category%', '([^&]+)');
+    add_rewrite_tag('%paged%', '([0-9]+)');
+
     add_rewrite_rule(
-        '^product-category/([^/]+)/page/([0-9]+)/?',
-        'index.php?product-category=$matches[1]&paged=$matches[2]',
+        '^product-category/([^/]+)/page/([0-9]+)/?$',
+        'index.php?taxonomy=product-category&term=$matches[1]&paged=$matches[2]',
+        'top'
+    );
+
+    add_rewrite_rule(
+        '^product-category/([^/]+)/?$',
+        'index.php?taxonomy=product-category&term=$matches[1]',
         'top'
     );
 }
 add_action('init', 'fix_product_category_pagination');
 
-add_action('init', function() {
-    add_rewrite_rule(
-        '^product-category/([^/]+)/page/([0-9]+)/?$',
-        'web/index.php?taxonomy=product-category&term=$matches[1]&paged=$matches[2]',
-        'top'
-    );
-});
-
-add_action('init', function() {
+register_activation_hook(__FILE__, function() {
+    fix_product_category_pagination();
     flush_rewrite_rules();
 });
