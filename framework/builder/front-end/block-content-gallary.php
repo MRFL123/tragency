f078625fb4c7eda030f66bbc2c11c81351784df8
Abanoub-N <abanoubnabil799@gmail.com>
2025-10-19 12:47:49 +0300

    working on products

--- framework/builder/front-end/block-content-gallary.php
+++ framework/builder/front-end/block-content-gallary.php
@@ -17,8 +17,8 @@ $gallery             = get_field('gallery');
             <div class="col-lg-5 mb-4 mb-lg-0">
                 <?php if ($heading): ?>
                     <div class="heading"><?= $heading ?></div>
+                    <div class="spacer-5"></div>
                 <?php endif; ?>
-                <div class="spacer-5"></div>
                 <?php if ($first_text_section): ?>
                 <div class="bg-white p-4 mb-4 shadow-sm">
                     <p><?= $first_text_section ?></p>
