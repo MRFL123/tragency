94c0fd3900fd3f85424f75f5217e5ab551c6fb8d
Abanoub-N <abanoubnabil799@gmail.com>
2025-10-19 12:18:58 +0300

    working on products

--- framework/builder/front-end/block-content-gallary.php
+++ framework/builder/front-end/block-content-gallary.php
@@ -4,6 +4,7 @@
  */
 
 $heading             = get_field('heading');
+$gallery_heading     = get_field('gallery_heading');
 $first_text_section  = get_field('first_text_section');
 $second_text_section = get_field('second_text_section');
 $gallery             = get_field('gallery');
@@ -11,41 +12,46 @@ $gallery             = get_field('gallery');
 
 <section class="custom-gallery py-4 overflow-hidden" >
     <div class="container">
-        <?php if ($heading): ?>
-        <h2 class="text-center mb-4"><?= $heading ?></h2>
-        <?php endif; ?>
 
-        <div class="row">
-        <div class="col-lg-4 mb-4 mb-lg-0">
-            <?php if ($first_text_section): ?>
-            <div class="bg-white p-4 mb-4 shadow-sm">
-                <p><?= $first_text_section ?></p>
-            </div>
-            <?php endif; ?>
-
-            <?php if ($second_text_section): ?>
-            <div class="bg-white p-4 shadow-sm ">
-                <p><?= $second_text_section ?></p>
-            </div>
-            <?php endif; ?>
-        </div>
-
-        <div class="col-lg-8">
-            <div class="row g-4">
-            <?php if ($gallery): ?>
-                <?php foreach ($gallery as $image): ?>
-                <div class="col-6">
-                    <div class="ratio ratio-4x3  overflow-hidden shadow-sm">
-                    <img
-                        src="<?= $image['url']; ?>"
-                        alt="<?= $image['alt']; ?>"
-                        class="w-100 h-100 object-fit-cover">
-                    </div>
+        <div class="row g-md-5">
+            <div class="col-lg-5 mb-4 mb-lg-0">
+                <?php if ($heading): ?>
+                    <div class="heading"><?= $heading ?></div>
+                <?php endif; ?>
+                <div class="spacer-20"></div>
+                <?php if ($first_text_section): ?>
+                <div class="bg-white p-4 mb-4 shadow-sm">
+                    <p><?= $first_text_section ?></p>
                 </div>
-                <?php endforeach; ?>
-            <?php endif; ?>
+                <?php endif; ?>
+
+                <?php if ($second_text_section): ?>
+                <div class="bg-white p-4 shadow-sm ">
+                    <p><?= $second_text_section ?></p>
+                </div>
+                <?php endif; ?>
             </div>
-        </div>
+
+            <div class="col-lg-7">
+                <?php if ($gallery_heading): ?>
+                    <div class="gallery_heading"><?= $gallery_heading ?></div>
+                <?php endif; ?>
+                <div class="spacer-30"></div>
+                <div class="row g-4">
+                <?php if ($gallery): ?>
+                    <?php foreach ($gallery as $image): ?>
+                    <div class="col-6">
+                        <div class="ratio ratio-4x3  overflow-hidden shadow-sm">
+                        <img
+                            src="<?= $image['url']; ?>"
+                            alt="<?= $image['alt']; ?>"
+                            class="w-100 h-100 object-fit-cover">
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                </div>
+            </div>
         </div>
     </div>
 </section>
