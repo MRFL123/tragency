953333e46de94166bee82a87cd5fdfbbadd8358b
Abanoub-N <abanoubnabil799@gmail.com>
2025-10-22 16:12:34 +0300

    Fix issues

--- resources/views/partials/single/content-single-post.blade.php
+++ resources/views/partials/single/content-single-post.blade.php
@@ -1,3 +1,17 @@
 <section class="single post">
-  {!! the_content() !!}
+  @if (has_post_thumbnail())
+    <div class="post-thumbnail mb-4 text-center">
+      {!! get_the_post_thumbnail(null, 'large', ['class' => 'w-100 rounded']) !!}
+    </div>
+  @endif
+
+  <div class="container">
+    <h1 class="h4">
+      {!! get_the_title() !!}
+    </h1>
+
+    <div class="content">
+      {!! the_content() !!}
+    </div>
+  </div>
 </section>
