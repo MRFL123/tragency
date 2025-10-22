97008d6f6c4d07aa68ee297bc5826daf044c9ed3
Abanoub-N <abanoubnabil799@gmail.com>
2025-10-22 16:07:59 +0300

    Fix issues

--- resources/views/partials/content-search.blade.php
+++ resources/views/partials/content-search.blade.php
@@ -1,9 +1,9 @@
 <article @php(post_class('mb-4 pb-3 border-bottom'))>
   <h2 class="h5">
     <a href="{{ get_permalink() }}" class="text-dark text-decoration-none">
-      {{ get_the_title() }}
+      {!! get_the_title() !!}
     </a>
   </h2>
   {{-- <p class="text-muted small mb-2">{{ get_the_date() }}</p> --}}
-  <div class="excerpt">{{ get_the_excerpt() }}</div>
+  <div class="excerpt">{!! get_the_excerpt() !!}</div>
 </article>
